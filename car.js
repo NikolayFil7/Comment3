4efa65d4e34f2b8bb114732201b6408af5074c1c
NikolayFil7 <krasavcheknik@protonmail.com>
2024-12-14 00:53:05 +0700

    Add files via upload

---
+++ car.js
@@ -0,0 +1,14 @@
+const carName = "Octavia"
+alert("Octavia")
+let maxCarSpeed = "200 km/h"
+alert("200 km/h")
+let carOwner = "Filippov"
+alert("Filippov")
+maxCarSpeed = "210 km/h"
+console.log("У машины изменилось скорость", maxCarSpeed)
+carOwner = "Schumacher"
+console.log("У машины изменился владелец", carOwner)
+maxCarSpeed = "180km/h"
+console.log("У машины изменилось скорость", maxCarSpeed)
+carOwner = "Statham"
+console.log("У машины изменилось скорость", carOwner)
